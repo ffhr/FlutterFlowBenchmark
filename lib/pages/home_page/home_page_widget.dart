2c55700445ef5ea0a9089bb55cad3406c0fda7a6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-02 18:25:46 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -77,7 +77,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
                     context.goNamedAuth('LoginPage', context.mounted);
                   },
-                  text: 'Logout 15',
+                  text: 'Logout 16',
                   options: FFButtonOptions(
                     height: 90.0,
                     padding:
